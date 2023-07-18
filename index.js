fa23e33e482f38d0ddb6aa77e0549a497257fca1
derrickkilimo <derrick.kilimo@student.moringaschool.com>
2023-07-18 21:14:58 +0300

    Finish project

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Code your solution in this file!
+// set companyName to Scuber
 const companyName = 'Scuber';
-
+// set mostProfitableNeighborhood to Chelsea
 let mostProfitableNeighborhood = 'Chelsea'
+// set companyCeo toSusan 
 let companyCeo = 'Susan Smith'
